b48e2a08e345eaac7ff7c4f3d6e5fe19c865f420
vcaquant <vcaquant@student.42.fr>
2015-12-30 18:28:27 +0100

    up

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,21 @@
 /*   By: vcaquant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/23 16:16:00 by vcaquant          #+#    #+#             */
-/*   Updated: 2015/12/30 15:59:36 by vcaquant         ###   ########.fr       */
+/*   Updated: 2015/12/30 18:27:55 by vcaquant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		free_str(char *str)
+int		
+{
+	
+}
+
+int		free_str(char *str, int ret)
 {
 	free(str);
-	return ();
+	return (ret);
 }
 
 int		get_next_line(int const fd, char ** line)
@@ -25,10 +30,12 @@ int		get_next_line(int const fd, char ** line)
 	
 	if (!line || (BUFF_SIZE <= 0))
 		return (-1);
-	if ((ret = read(fd, buff, BUFF_SIZE)))
+	if ()
 	{
-		buff[ret] = '\0';
-
+		while ((ret = read(fd, buff, BUFF_SIZE)))
+		{
+			buff[ret] = '\0';
+		}
 	}
 	return (ret);
 }
