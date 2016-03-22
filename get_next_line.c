6645382f70483f88828678da7f75f52edd1c9e66
Victorien CAQUANT <vcaquant@e2r1p10.42.fr>
2016-03-22 16:30:20 +0100

    segfault

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vcaquant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/23 16:16:00 by vcaquant          #+#    #+#             */
-/*   Updated: 2016/03/21 16:44:16 by vcaquant         ###   ########.fr       */
+/*   Updated: 2016/03/22 16:29:43 by vcaquant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ int		ft_strnlen(char *str, char c)
 
 int		get_next_line(int const fd, char **line)
 {
-	static char		buff[BUFF_SIZE + 1];
+	static char		buff[2] = "";
 	int				i;
 
 	ft_putstr("hey\n");
@@ -42,26 +42,32 @@ int		get_next_line(int const fd, char **line)
 	if (!line || (BUFF_SIZE <= 0))
 		return (-1);
 	ft_putstr("premier if\n");
-	while (read(fd, buff, 1))
+	while (read(fd, buff, BUFF_SIZE) > 0)
 	{
-		buff[BUFF_SIZE] = '\0';
-		ft_putstr("avant");
-		*line = malloc((ft_strnlen(buff, '\n')) * sizeof(char));
-		ft_putstr("apres");
+//		ft_putstr(buff);
+		ft_putstr("avant\n");
+		*line = malloc(100 * sizeof(char));
+		ft_putstr("apres\n");
 		*line[0] = '\0';
 		ft_putstr("premier while\n");
-		if (ft_strchr(buff, '\n') != NULL)
+		if ((ft_strchr(buff, '\n')) != NULL || (ft_strchr(buff, '\0')) != NULL)
 		{
-			while (buff[i] != '\n' || buff[i] != '\0')
+			ft_putstr("rentre\n");
+			while (buff[i] != '\n' && buff[i] != '\0')
 			{
-				ft_strncat(*line, buff, 1);
-				ft_putstr(*line);
-				ft_putstr("je sais pas");
+				ft_strncat(line[i], buff, 1);
+				ft_putstr("\033[032;31m");
+				ft_putstr(line[i]);
+				ft_putstr("\033[0m");
+				ft_putchar('\n');
+				ft_putstr("je sais pas\n");
+				i++;
+				ft_putstr("avant 2eme\n");
 			}
 			if (buff[i] == '\n' || buff[i] == '\0')
 				return (1);
-			i++;
 		}
+		ft_putstr("rentre pas\n");
 //		if (ret < 0)
 //			return (free_str(buff, -1));
 	}
