12af71414c3e044854b248f5f18723480347d02d
Victorien CAQUANT <vcaquant@e2r3p21.42.fr>
2016-04-04 17:25:10 +0200

    salut

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: vcaquant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/23 16:16:00 by vcaquant          #+#    #+#             */
-/*   Updated: 2016/03/23 18:30:53 by vcaquant         ###   ########.fr       */
+/*   Updated: 2016/04/04 16:31:48 by vcaquant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,16 @@ int		ft_strnlen(char *str, char c)
 	return (i);
 }
 
-t_data	g_data(int const fd, t_list **list)
-{
-	t_data		*data;
-
-
-}
+//t_data	g_data(int const fd)
+//{
+//	t_data		*data;
+//}
 
 int		get_next_line(int const fd, char **line)
 {
 	static char		buff[2] = "";
 	int				i;
+	char			**data;
 
 	ft_putstr("hey\n");
 	i = 0;
@@ -51,8 +50,9 @@ int		get_next_line(int const fd, char **line)
 	ft_putstr("premier if\n");
 	while (read(fd, buff, BUFF_SIZE) > 0)
 	{
-//		ft_putstr(buff);
+		ft_putstr(buff);
 		ft_putstr("avant\n");
+		data = malloc(100 * sizeof(char));
 		*line = malloc(100 * sizeof(char));
 		ft_putstr("apres\n");
 		*line[0] = '\0';
@@ -62,18 +62,23 @@ int		get_next_line(int const fd, char **line)
 			ft_putstr("rentre\n");
 			while (buff[i] != '\n' && buff[i] != '\0')
 			{
-				ft_strncat(*line, buff, 1);
+				data[i] = ft_strsub(buff, i, 1);
 				ft_putstr("\033[032;31m");
-				ft_putstr(*line);
+				ft_putstr(data[i]);
 				ft_putstr("\033[0m");
 				ft_putchar('\n');
 				ft_putstr("je sais pas\n");
 				i++;
-				line++;
 				ft_putstr("avant 2eme\n");
 			}
 			if (buff[i] == '\n' || buff[i] == '\0')
+			{
+				ft_strcpy(*line, *data);
+				ft_putstr("\033[033;33m");
+				ft_putstr(*line);
+				ft_putstr("\033[0m");
 				return (1);
+			}
 		}
 		ft_putstr("rentre pas\n");
 //		if (ret < 0)
