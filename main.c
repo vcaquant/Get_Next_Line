37bb2e1197be8b4430957d87770cefb711d0086c
Victorien CAQUANT <vcaquant@e2r2p23.42.fr>
2016-05-11 18:17:16 +0200

    modif main

--- main.c
+++ main.c
@@ -6,10 +6,10 @@
 /*   By: vcaquant <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/12/23 16:10:02 by vcaquant          #+#    #+#             */
-/*   Updated: 2016/05/10 14:16:09 by vcaquant         ###   ########.fr       */
+/*   Updated: 2016/05/11 18:14:46 by vcaquant         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
+/*
 #include "get_next_line.h"
 #include <stdio.h>
 #include <fcntl.h>
@@ -23,7 +23,7 @@ int		main(void)
 	close(fd);
 	return (0);
 }
-/*
+*/
 #include "get_next_line.h"
 //#include "libft.h"
 #include <sys/types.h>
@@ -37,24 +37,52 @@ int     main(int ac, char **av)
 	int     fd;
 	char    *line;
 	int		i;
+	int		j;
+	int		k;
 
-	(void)ac;
-	fd = open(av[1], O_RDONLY);
-	if (fd == -1)
+	j = ac;
+	k = 1;
+	if (j < 2)
 	{
-		ft_putstr("open() error");
-		return (1);
-	}
-	while ((i = get_next_line((int const)fd, &line)) > 0)
-	{
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
+		{
+			ft_putstr("open() error");
+			return (1);
+		}
+		while ((i = get_next_line((int const)fd, &line)) > 0)
+		{
+			ft_putchar('[');
+			ft_putnbr(i);
+			ft_putchar(']');
+			ft_putendl(line);							//affiche tout le fichier
+			free(line);
+		}
 		ft_putchar('[');
 		ft_putnbr(i);
 		ft_putchar(']');
-		ft_putendl(line);							//affiche tout le fichier
-		free(line);
 	}
+	while (j >= 2)
+	{
+		fd = open(av[k], O_RDONLY);
+		if (fd == -1)
+		{
+			ft_putstr("open() error");
+			return (1);
+		}
+		while ((i = get_next_line((int const)fd, &line)) > 0)
+		{
+			ft_putchar('[');
+			ft_putnbr(i);
+			ft_putchar(']');
+			ft_putendl(line);							//affiche tout le fichier
+			free(line);
+		}
 		ft_putchar('[');
 		ft_putnbr(i);
 		ft_putchar(']');
+		k++;
+		j--;
+	}
 	return (1);
-}*/
+}
